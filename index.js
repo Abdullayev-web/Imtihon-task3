842fe65cfb3ec5a9fa54bd52664d16ee2ed0db93
Abdullayev-web <159528597+Abdullayev-web@users.noreply.github.com>
2024-02-14 14:42:09 +0500

    #

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-let button = document.addEventListener(button)
+let btn1 = document.querySelector('.add')
+let btn2 = document.querySelector('.del')
+let blok = document.querySelector('.blok')
 
+btn1.addEventListener('click', ()=>{
+    blok.classList.add('blok')
+})
+btn2.addEventListener('click', ()=>{
+    blok.classList.remove('blok')
+})
